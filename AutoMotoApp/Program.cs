d10e3743f718553c547a01f76c392f497d132a74
DagmaraJo <djodko@gmail.com>
2024-06-28 19:21:40 +0200

    let's start by initializing our own stack

---
+++ AutoMotoApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using AutoMotoApp;
+
+var stack = new BasicStack();
+stack.Push(4.5);
+stack.Push(45);
+stack.Push(245);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+Console.WriteLine($"Sum: {sum}");
